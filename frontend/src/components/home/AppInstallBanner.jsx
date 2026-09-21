0d2e921d49e82971bcadb006be422cd3dd35f5dc
GoSubsidy <info@gosubsidy.com>
2026-09-21 13:08:49 +0530

    Update AppInstallBanner.jsx

--- frontend/src/components/home/AppInstallBanner.jsx
+++ frontend/src/components/home/AppInstallBanner.jsx
@@ -3,7 +3,7 @@ import "./AppInstallBanner.css";
 
 // GoSubsidy Production Release APK
 const APK_DOWNLOAD_URL =
-  "https://drive.google.com/uc?export=download&id=1GCBHhShsUqcbYnREXGLLYiKV_MrZonKM";
+  "https://drive.google.com/uc?export=download&id=1rz9xicA5duesU73375hELiDfBdJqc-v8";
 
 const DISMISS_KEY = "gosubsidy_app_install_dismissed";
 
